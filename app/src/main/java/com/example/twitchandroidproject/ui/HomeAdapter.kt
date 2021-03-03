d8739927305022830bdbb3aa8a2729c806a38f46
tanglil@twitch.tv <tanglil@147dda083db9.ant.amazon.com>
2021-03-02 22:09:25 -0800

    Create grid layout recycler view with hard-coded objects and clickable cards

---
+++ app/src/main/java/com/example/twitchandroidproject/ui/HomeAdapter.kt
@@ -0,0 +1,68 @@
+package com.example.twitchandroidproject.ui
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.example.twitchandroidproject.R
+
+class HomeAdapter(private val people: Array<Person>) : RecyclerView.Adapter<HomeAdapter.ViewHolder>() {
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder).
+     */
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val personName: TextView
+        val personAge: TextView
+        val preferredInterest1: TextView
+        val preferredInterest2: TextView
+        val preferredInterest3: TextView
+        val personDistance: TextView
+
+        init {
+            // Define click listener for the ViewHolder's View.
+            personName = view.findViewById(R.id.person_name)
+            personAge = view.findViewById(R.id.person_age)
+            preferredInterest1 = view.findViewById(R.id.preferred_interest_1)
+            preferredInterest2 = view.findViewById(R.id.preferred_interest_2)
+            preferredInterest3 = view.findViewById(R.id.preferred_interest_3)
+            personDistance = view.findViewById(R.id.person_distance)
+        }
+    }
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        // Create a new view, which defines the UI of the list item
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.person_card, viewGroup, false)
+        val holder =  ViewHolder(view)
+
+        holder.itemView.setOnClickListener(
+            fun (v: View) {
+//                Log.d("TAG", "position = " + holder.getAdapterPosition())
+                view.findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            }
+        );
+
+        return holder
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        viewHolder.personAge.text = people[position].age
+        viewHolder.personName.text = people[position].name
+        viewHolder.personDistance.text = people[position].distance
+        viewHolder.preferredInterest1.text = people[position].preferredInterest1
+        viewHolder.preferredInterest2.text = people[position].preferredInterest2
+        viewHolder.preferredInterest3.text = people[position].preferredInterest3
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = people.size
+
+}
--- app/src/main/java/com/example/twitchandroidproject/ui/SecondFragment.kt
+++ app/src/main/java/com/example/twitchandroidproject/ui/SecondFragment.kt
@@ -1,32 +1,38 @@
 package com.example.twitchandroidproject.ui
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import com.example.twitchandroidproject.R
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class SecondFragment : Fragment(), View.OnClickListener {
+    lateinit var fab: ExtendedFloatingActionButton
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        return inflater.inflate(R.layout.profile_layout, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        }
+        fab = view.findViewById(R.id.add_friend_fab)
+        fab.setOnClickListener(this)
+
+        //TODO: make call to database to get actual data
+    }
+
+    override fun onClick(v: View) {
+        //TODO: Implement button behavior
+        Toast.makeText(context, "Execute action", Toast.LENGTH_SHORT).show()
     }
 }
