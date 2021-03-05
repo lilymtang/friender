cdccbda3cbcf7621a5d6ce593757330b115a8945
Viktorija Urzika <viktorija.urzika@gmail.com>
2021-03-05 15:12:22 -0800

    TA-24 Add option to get available interests from Repository

--- app/src/main/java/com/example/twitchandroidproject/repository/FrienderRepository.kt
+++ app/src/main/java/com/example/twitchandroidproject/repository/FrienderRepository.kt
@@ -1,9 +1,12 @@
 package com.example.twitchandroidproject.repository
 
+import android.content.Context
+import com.example.twitchandroidproject.R
 import com.example.twitchandroidproject.di.DispatcherProvider
 import com.example.twitchandroidproject.repository.api.GeolocationApiService
 import com.example.twitchandroidproject.repository.database.FrienderDatabase
 import com.example.twitchandroidproject.repository.model.UserProfile
+import dagger.hilt.android.qualifiers.ApplicationContext
 import io.reactivex.rxjava3.core.Observable
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
@@ -11,6 +14,7 @@ import javax.inject.Singleton
 
 @Singleton
 class FrienderRepository @Inject constructor(
+    @ApplicationContext private val context: Context,
     private val database: FrienderDatabase,
     private val geolocationService: GeolocationApiService,
     private val dispatcherProvider: DispatcherProvider
@@ -40,4 +44,12 @@ class FrienderRepository @Inject constructor(
                 throw IllegalArgumentException("User profile type is not Current User")
             }
         }
+
+    /**
+     * Provides the list of all the interests available for selection
+     *
+     * @return List of interest names
+     */
+    fun getAvailableInterests(): List<String> =
+        context.resources.getStringArray(R.array.available_interests).toList().sorted()
 }
