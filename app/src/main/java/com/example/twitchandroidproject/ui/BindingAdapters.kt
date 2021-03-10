94123f47fa9b04c4996b1a74fb8acab4bda9e043
Lily Tang <--global>
2021-03-09 22:30:26 -0800

    Add preferredInterestListByIndex to binding adapter to show preferred interests

--- app/src/main/java/com/example/twitchandroidproject/ui/BindingAdapters.kt
+++ app/src/main/java/com/example/twitchandroidproject/ui/BindingAdapters.kt
@@ -19,5 +19,10 @@ class BindingAdapters {
             view.text = years.toString()
         }
 
+        @BindingAdapter(value=["preferredInterest", "index"])
+        @JvmStatic
+        fun preferredInterestListByIndex(view: TextView, preferredInterests: List<String>, i: Int) {
+            view.text = preferredInterests[i]
+        }
     }
 }
