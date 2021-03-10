f0515a751d90ad128ecaffca1bbe8422ba77a0e4
Lily Tang <--global>
2021-03-09 19:24:12 -0800

    Use repository method to show name field in person card

--- app/src/main/java/com/example/twitchandroidproject/ui/HomeFragmentViewModel.kt
+++ app/src/main/java/com/example/twitchandroidproject/ui/HomeFragmentViewModel.kt
@@ -1,13 +1,19 @@
 package com.example.twitchandroidproject.ui
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.LiveDataReactiveStreams
 import androidx.lifecycle.ViewModel
 import com.example.twitchandroidproject.repository.FrienderRepository
+import com.example.twitchandroidproject.repository.model.UserProfile
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.rxjava3.core.BackpressureStrategy
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeFragmentViewModel @Inject constructor(
-    private val frienderRepository: FrienderRepository
+        private val frienderRepository: FrienderRepository
 ) : ViewModel() {
-
+    val userProfiles: LiveData<List<UserProfile>> = LiveDataReactiveStreams.fromPublisher(frienderRepository
+            .getOtherUsersNearby()
+            .toFlowable(BackpressureStrategy.BUFFER))
 }
